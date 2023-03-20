32cb136edab821739b186ba52f13dc21c97db0e5
shubhadip <shubhadip.maity@gmail.com>
2023-03-20 16:40:42 +0530

    refactor: removed urls

--- lighthouserc.js
+++ lighthouserc.js
@@ -11,7 +11,7 @@ module.exports = {
         "settings": {
           "chromeFlags": "--no-sandbox --headless"
         },
-        "url": ["http://localhost:5173","http://localhost:5173/about",]
+        // "url": ["http://localhost:5173","http://localhost:5173/about",]
       },
       "assert": {
         "assertions": {
